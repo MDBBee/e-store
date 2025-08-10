1852be36a1d0640df58c5391768cea3cc80b9604
MDBBee <bobbyugbebor@gmail.com>
2025-08-10 11:47:12 +0300

    Docs1

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -5,6 +5,12 @@ const Product = require('../models/Product');
 const Order = require('../models/Order');
 const checkPermissions = require('../utils/checkPermissions');
 
+// Fake stripe sdk
+const fakeStripeApi = async (payload) => {
+  const client_secret = 'someRandomValue';
+  return { client_secret, amount: payload.amount };
+};
+
 const createOrder = asyncWrapperMiddleWare(async (req, res, next) => {
   const { items: cartItems, tax, shippingFee } = req.body;
 
@@ -46,28 +52,65 @@ const createOrder = asyncWrapperMiddleWare(async (req, res, next) => {
       product: _id,
     };
 
-    orderItems = [...orderItems, ...singleOrderItem];
-
-    console.log(singleOrderItem);
+    orderItems = [...orderItems, singleOrderItem];
+    subTotal += item.amount * price;
   }
 
-  res.status(201).json('createOrder');
+  const total = tax + shippingFee + subTotal;
+  //   Fake stripe process
+  const paymentIntent = await fakeStripeApi({
+    amount: total,
+    currency: 'usd',
+  });
+
+  const order = await Order.create({
+    orderItems,
+    total,
+    subtotal: subTotal,
+    tax,
+    shippingFee,
+    clientSecret: paymentIntent.client_secret,
+    user: req.user.userId,
+  });
+
+  res.status(201).json({ order, clientSecret: order.clientSecret });
 });
 
 const getAllOrders = asyncWrapperMiddleWare(async (req, res, next) => {
-  res.status(201).json('getAllOrders');
+  const orders = await Order.find({});
+  res.status(200).json({ orders, count: orders.length });
 });
 
 const getSingleOrder = asyncWrapperMiddleWare(async (req, res, next) => {
-  res.status(201).json('getSingleOrder');
+  const { id: orderId } = req.params;
+  const order = await Order.findOne({ _id: orderId });
+  if (!order) {
+    return next(customErrorBuilder(`No order with id : ${orderId}`));
+  }
+  checkPermissions(req.user, order.user);
+  res.status(200).json({ order });
 });
 
 const getCurrentUserOrders = asyncWrapperMiddleWare(async (req, res, next) => {
-  res.status(201).json('getCurrentUserOrders');
+  const orders = await Order.find({ user: req.user.userId });
+  res.status(200).json({ orders, count: orders.length });
 });
 
 const updateOrder = asyncWrapperMiddleWare(async (req, res, next) => {
-  res.status(201).json('updateOrder');
+  const { id: orderId } = req.params;
+  const { paymentIntentId } = req.body;
+
+  const order = await Order.findOne({ _id: orderId });
+  if (!order) {
+    return next(customErrorBuilder(`No order with id : ${orderId}`));
+  }
+  checkPermissions(req.user, order.user);
+
+  order.paymentIntentId = paymentIntentId;
+  order.status = 'paid';
+  await order.save();
+
+  res.status(200).json({ order });
 });
 
 module.exports = {
